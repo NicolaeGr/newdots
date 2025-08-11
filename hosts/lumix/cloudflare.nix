258fa92a1fb1ed6fe95eaccceef15089437a5ebd
NicolaeGr <114419701+NicolaeGr@users.noreply.github.com>
2025-08-11 23:59:47 +0300

    Updated cloudflare

--- hosts/lumix/cloudflare.nix
+++ hosts/lumix/cloudflare.nix
@@ -9,7 +9,7 @@
       "8b63fee5-4681-49c5-bb56-1709c121d52a" = {
         credentialsFile = config.sops.secrets."cloudflare_cred".path;
         ingress = {
-          "mc.electrolit.biz" = "http://localhost:25565";
+          "mc.electrolit.biz" = "tcp://localhost:25565";
           "*.electrolit.biz" = "http://localhost:80";
         };
         default = "http_status:404";
