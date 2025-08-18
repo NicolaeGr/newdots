687d5f02cf93f934778c8fc0bdcc705e3050eca3
NicolaeGr <114419701+NicolaeGr@users.noreply.github.com>
2025-08-18 20:36:24 +0300

    removed unnecesary tunnels

--- hosts/lumix/cloudflare.nix
+++ hosts/lumix/cloudflare.nix
@@ -1,22 +1,6 @@
 { config, pkgs, ... }:
 {
-  sops.secrets."cloudflared_cred" = { };
   sops.secrets."cloudflare_api_key" = { };
-
-  services.cloudflared = {
-    enable = true;
-    package = pkgs.unstable.cloudflared;
-    tunnels = {
-      "8b63fee5-4681-49c5-bb56-1709c121d52a" = {
-        credentialsFile = config.sops.secrets."cloudflared_cred".path;
-        ingress = {
-          "mc.electrolit.biz" = "tcp://localhost:25565";
-          "*.electrolit.biz" = "http://localhost:80";
-        };
-        default = "http_status:404";
-      };
-    };
-  };
 
   services.cloudflare-dyndns = {
     enable = true;
