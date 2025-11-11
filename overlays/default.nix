4ddabfd33bd022e82d1db2127cc90f18c6bf1b02
NicolaeGr <114419701+NicolaeGr@users.noreply.github.com>
2025-11-11 14:15:20 +0200

    added back config to overlays

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,9 +5,19 @@
   modifications = final: prev: { };
 
   stable-packages = final: _prev: {
-    stable = import inputs.nixpkgs-stable { system = final.system; };
+    stable = import inputs.nixpkgs-stable {
+      system = final.system;
+      config = {
+        allowUnfree = true;
+      };
+    };
   };
   unstable-packages = final: _prev: {
-    unstable = import inputs.nixpkgs-unstable { system = final.system; };
+    unstable = import inputs.nixpkgs-unstable {
+      system = final.system;
+      config = {
+        allowUnfree = true;
+      };
+    };
   };
 }
